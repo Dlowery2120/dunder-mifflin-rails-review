71be06b40cff5a37e25d8d083b76c8a00fdef10f
Devante Lowery <dlowery2120@gmail.com>
2021-03-24 18:07:13 -0500

    Working on unique alias

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,56 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show 
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+       
+        @employee = Employee.new(employee_params)
+        @employee.save
+
+        redirect_to @employee
+    end
+
+    def edit 
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        
+        aliases = @employees.all.map {|e| e.alias}.uniq
+            if aliases.include?(params[:alias])
+                render :edit
+            else
+                @employee.update(employee_params) 
+                redirect_to @employee       
+            end
+    end
+ 
+    
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(
+            :first_name,
+            :last_name,
+            :alias,
+            :title,
+            :office,
+            :dog_id,
+            :img_url
+        )
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,8 @@
 class Employee < ApplicationRecord
+    #An employee can be connected to a single dog
+    belongs_to :dog
+
+    def unique_alias
+        
+    end
 end
